9b022fe2014528abe7bb873eaa04892121a9c875
Muhammad Irfan <irfanmirza686@gmail.com>
2024-06-24 10:51:03 +0500

    update products schema

--- database/migrations/2022_08_23_133036_create_products_table.php
+++ database/migrations/2022_08_23_133036_create_products_table.php
@@ -16,6 +16,7 @@ class CreateProductsTable extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('type')->nullable();
+            $table->string('v2_productID')->nullable();
             $table->string('gcpGLNID')->nullable();
             $table->string('productnameenglish')->nullable();
             $table->string('productnamearabic')->nullable();
